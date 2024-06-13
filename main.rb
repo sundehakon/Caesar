44fde116091a9078c818212d341c7c8f2191cb75
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-13 15:15:48 +0200

    Format input in ascii value and increment

--- main.rb
+++ main.rb
@@ -1,6 +1,21 @@
-print "Enter word: "
-input = gets.chomp
+def caesar
+    print "Enter word: "
+    input = gets.chomp
+    print "Enter increment: "
+    increment = gets.chomp.to_i
 
-input.split("").each do |i|
-    puts i.succ
+    input.split("").each do |i|
+        ascii = i.ord
+        if ascii.between?(65, 90)  
+            ascii += 6
+            ascii -= 25 if ascii > 90
+        elsif ascii.between?(97, 122) 
+            ascii += 5
+            ascii -= 26 if ascii > 122 
+        end
+        print ascii.chr
+    end
+    puts
 end
+
+caesar
