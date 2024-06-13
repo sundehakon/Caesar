935feb148ddcef85b918f9a85867981ae5a08778
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-13 18:05:56 +0200

    Add ascii art title and fix small issues

--- main.rb
+++ main.rb
@@ -3,7 +3,17 @@ def caesar
     increment = nil # Define increment as a global variable
     loop do
       system("clear") || system("cls")  # Clear the screen
-      puts "\nType 'view' to see encrypted string"
+      title = "
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░ ░▒▓██████▓▒░░▒▓████████▓▒░░▒▓███████▓▒░░▒▓██████▓▒░░▒▓███████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░ ░▒▓██████▓▒░░▒▓█▓▒░      ░▒▓████████▓▒░▒▓██████▓▒░  ░▒▓██████▓▒░░▒▓████████▓▒░▒▓███████▓▒░  
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░  ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░             ░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░░▒▓██████▓▒░░▒▓███████▓▒░   ░▒▓█▓▒░    ░▒▓██████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓████████▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░                                                                                                                                                                                                         
+"
+      puts title  
+      puts "Type 'view' to see encrypted string"
       puts "Type 'see' to see decrypted string"
       puts "Type 'add' to add new string"
       puts "Type 'exit' to exit"
@@ -13,33 +23,33 @@ def caesar
   
       if menu_input == "add"
         input = nil
-    
+  
         while input.nil? || input.match?(/\d/) # Fetch word/password to encrypt
-          system("clear") || system("cls")  # Clear the screen
+          system("clear") || system("cls") 
           print "Enter word (cannot contain integers): "
           input = gets.chomp
-    
+  
           if input.match?(/\d/)
             puts "Invalid input. The word cannot contain integers. Please try again."
           end
         end
-    
+  
         increment = nil
-    
+  
         while increment.nil? || increment < -5 || increment > 5 # Fetch preferred increment value
           print "Enter shift amount (-5 - 5): "
           increment = gets.chomp.to_i
-    
+  
           if increment < -5 || increment > 5
             puts "Invalid increment. It must be between -5 and 5. Please try again."
           end
         end
-    
+  
         encrypted_string = "" # Initialize as an empty string
-    
+  
         input.split("").each do |i| # Convert string to char and then array for incrementation
           ascii = i.ord
-    
+  
           if ascii.between?(65, 90)  # Lowercase letters
             ascii += increment
             ascii -= increment + 21 if ascii > 90
@@ -48,77 +58,78 @@ def caesar
             ascii -= increment + 21 if ascii > 122 
           end
   
-          puts ascii
           encrypted_string += ascii.chr
         end
-    
+  
         puts "Your string is encrypted!"
   
         print "Go to menu? (y/n): "
         retry_input = gets.chomp
         if retry_input == "y"
-            next
+          next
         elsif retry_input == "n"
-            break
+          break
         else
-            puts "Invalid input. Please try again."
+          puts "Invalid input. Please try again."
         end
       elsif menu_input == "view"
         if encrypted_string.empty?  # Check if encrypted_string is empty
-            puts "No encrypted string available. Please add a string first."
-            print "Go to menu? (y/n): "
-            retry_input = gets.chomp
-            if retry_input == "y"
-                next
-            elsif retry_input == "n"
-                break
-            else
-                puts "Invalid input. Please try again."
-            end
-        else
-          puts encrypted_string
-    
+          puts "No encrypted string available. Please add a string first."
           print "Go to menu? (y/n): "
           retry_input = gets.chomp
           if retry_input == "y"
-              next
+            next
           elsif retry_input == "n"
-              break
+            break
           else
-              puts "Invalid input. Please try again."
+            puts "Invalid input. Please try again."
+          end
+        else
+          system("clear") || system("cls") 
+          puts encrypted_string
+  
+          print "Go to menu? (y/n): "
+          retry_input = gets.chomp
+          if retry_input == "y"
+            next
+          elsif retry_input == "n"
+            break
+          else
+            puts "Invalid input. Please try again."
           end
         end
       elsif menu_input == "see"
         if encrypted_string.empty?
-            puts "No decrypted string available. Please add a string first."
-            print "Go to menu? (y/n): "
-            retry_input = gets.chomp
-            if retry_input == "y"
-                next
-            elsif retry_input == "n"
-                break
-            else
-                puts "Invalid input. Please try again."
-            end
+          puts "No decrypted string available. Please add a string first."
+          print "Go to menu? (y/n): "
+          retry_input = gets.chomp
+          if retry_input == "y"
+            next
+          elsif retry_input == "n"
+            break
+          else
+            puts "Invalid input. Please try again."
+          end
         else
-            decrypted_string = ""
-            encrypted_string.split("").each do |i| # Decrypts the stored encrypted string
-                ascii = i.ord
-                ascii -= increment
-                decrypted_string = ascii.chr
-                print decrypted_string
-            end
-            puts
+          decrypted_string = ""
+          encrypted_string.split("").each do |i| # Decrypts the stored encrypted string
+            ascii = i.ord
+            ascii -= increment
+            decrypted_string += ascii.chr
+          end
+          system("clear") || system("cls") 
+          puts decrypted_string
+          puts
   
-            print "Go to menu? (y/n): "
-            retry_input = gets.chomp
-            if retry_input == "y"
-                next
-            elsif retry_input == "n"
-                break
-            else
-                puts "Invalid input. Please try again."
-            end
+          print "Go to menu? (y/n): "
+          retry_input = gets.chomp
+          if retry_input == "y"
+            next
+          elsif retry_input == "n"
+            break
+          else
+            puts "Invalid input. Please try again."
+          end
         end
       elsif menu_input == "exit"
         break
