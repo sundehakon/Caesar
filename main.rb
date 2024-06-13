430ee834792d8d78f292b8404538942b851c2147
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-13 15:49:43 +0200

    Wrap code in do loop

--- main.rb
+++ main.rb
@@ -1,47 +1,61 @@
 def caesar
-
-puts "Type 'view' to see encrypted strings"
-puts "Type 'add' to add new string"
-puts "Type 'exit' to exit"
-
-menu_input = gets.chomp
-
-if menu_input == "add"
-    input = nil
-
-    while input.nil? || input.match?(/\d/) # Fetch word/password to encrypt
-        print "Enter word (cannot contain integers): "
-    input = gets.chomp
-
-        if input.match?(/\d/)
+    loop do
+      puts "Type 'view' to see encrypted strings"
+      puts "Type 'add' to add new string"
+      puts "Type 'exit' to exit"
+  
+      menu_input = gets.chomp
+  
+      if menu_input == "add"
+        input = nil
+  
+        while input.nil? || input.match?(/\d/) # Fetch word/password to encrypt
+          print "Enter word (cannot contain integers): "
+          input = gets.chomp
+  
+          if input.match?(/\d/)
             puts "Invalid input. The word cannot contain integers. Please try again."
+          end
         end
-    end
-
-    increment = nil
-
-    while increment.nil? || increment < -5 || increment > 5 # Fetch prefered increment value
-        print "Enter shift amount (-5 - 5): "
-        increment = gets.chomp.to_i
-
-        if increment < -5 || increment > 5
+  
+        increment = nil
+  
+        while increment.nil? || increment < -5 || increment > 5 # Fetch prefered increment value
+          print "Enter shift amount (-5 - 5): "
+          increment = gets.chomp.to_i
+  
+          if increment < -5 || increment > 5
             puts "Invalid increment. It must be between -5 and 5. Please try again."
+          end
         end
-    end
-
-    input.split("").each do |i| # Convert string to char and then array for incrementation
-        ascii = i.ord
-
-        if ascii.between?(65, 90)  # Lowercase letters
+  
+        encrypted_string = ""
+  
+        input.split("").each do |i| # Convert string to char and then array for incrementation
+          ascii = i.ord
+  
+          if ascii.between?(65, 90)  # Lowercase letters
             ascii += increment
             ascii -= increment + 21 if ascii > 90
-        elsif ascii.between?(97, 122) # Uppercase letters
+          elsif ascii.between?(97, 122) # Uppercase letters
             ascii += increment
             ascii -= increment + 21 if ascii > 122 
+          end
+  
+          encrypted_string += ascii.chr
         end
+  
+        puts "Your string is encrypted: #{encrypted_string}"
+      elsif menu_input == "view"
+        # Code to view decrypted strings
+        puts "View decrypted strings"
+      elsif menu_input == "exit"
+        break
+      else
+        puts "Invalid input. Please try again."
+      end
     end
-    puts "Your string is encrypted!"
-end
-end
-
-caesar
+  end
+  
+  caesar
+  
