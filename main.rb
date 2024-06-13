6d3d4298d92fb396764aae1517816ddaf51a0993
sundehakon <142574795+sundehakon@users.noreply.github.com>
2024-06-13 15:35:33 +0200

    Add while loops for checking word and increment input

--- main.rb
+++ main.rb
@@ -1,21 +1,40 @@
 def caesar
-    print "Enter word: "
-    input = gets.chomp
-    print "Enter increment: "
-    increment = gets.chomp.to_i
+input = nil
 
-    input.split("").each do |i|
-        ascii = i.ord
-        if ascii.between?(65, 90)  
-            ascii += 6
-            ascii -= 25 if ascii > 90
-        elsif ascii.between?(97, 122) 
-            ascii += 5
-            ascii -= 26 if ascii > 122 
+    while input.nil? || input.match?(/\d/) # Fetch word/password to encrypt
+        print "Enter word (cannot contain integers): "
+    input = gets.chomp
+
+        if input.match?(/\d/)
+            puts "Invalid input. The word cannot contain integers. Please try again."
         end
+    end
+
+    increment = nil
+
+    while increment.nil? || increment < -5 || increment > 5 # Fetch prefered increment value
+        print "Enter shift amount (-5 - 5): "
+        increment = gets.chomp.to_i
+
+        if increment < -5 || increment > 5
+            puts "Invalid increment. It must be between -5 and 5. Please try again."
+        end
+    end
+
+    input.split("").each do |i| # Convert string to char and then array for incrementation
+        ascii = i.ord
+
+        if ascii.between?(65, 90)  # Lowercase letters
+            ascii += increment
+            ascii -= increment + 21 if ascii > 90
+        elsif ascii.between?(97, 122) # Uppercase letters
+            ascii += increment
+            ascii -= increment + 21 if ascii > 122 
+        end
+
         print ascii.chr
     end
     puts
 end
 
-caesar
+caesar()
